2cf8d4f6702a54d00b2c4dc7bda1dbba370ab708
sep4ever <sepfourever@gmail.com>
2026-03-19 15:07:45 +0500

    Initial commit 2
    Made some base features like moving in x-z axis, added camera rotation, pixelating shader and breathing animation.

---
+++ Assets/Player.cs
@@ -0,0 +1,72 @@
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+
+public class Player : MonoBehaviour
+{
+    [Header("Movement.")]
+    Rigidbody playerRB;
+    [SerializeField] Vector3 playerInput;
+    [SerializeField] float playerSpeed = 10f;
+
+    [Header("Camera.")]
+
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    public float yaw = 0.0f;
+    public float pitch = 0.0f;
+
+    [SerializeField] Transform cameraTransform;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateCamera();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void RotateCamera()
+    {
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        pitch = Mathf.Clamp(pitch, -90f, 90f);
+
+        cameraTransform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+    }
+
+    void Move()
+    {
+        playerInput = Vector3.zero;
+        playerInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+
+        Vector3 camForward = cameraTransform.forward;
+        camForward.y = 0f;
+        camForward.Normalize();
+
+        Vector3 camRight = cameraTransform.right;
+        camRight.y = 0f;
+        camRight.Normalize();
+
+        // Движение в направлении взгляда: вперед/назад по camForward, влево/вправо по camRight
+        Vector3 move = camRight * playerInput.x + camForward * playerInput.z;
+
+        // Нормализуем, чтобы диагональ не дала больше скорости
+        if (move.sqrMagnitude > 1f) move.Normalize();
+
+        Vector3 velocity = move * playerSpeed * Time.fixedDeltaTime;
+
+        playerRB.MovePosition(playerRB.position + velocity);
+    }
+}
